7f405e3fc20fb048033dc3217945d986c029fc85
Felipe Borsato Tostes <fborsatotostes@gmail.com>
2025-04-21 21:13:11 -0300

    v1

---
+++ contador/src/Contador.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException exception) {
+			System.out.println("Primeiro parâmetro deve ser menor que o segundo parâmetro!");
+		}
+		terminal.close();
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		if (parametroUm >= parametroDois)
+			throw new ParametrosInvalidosException();
+		int contagem = parametroDois - parametroUm;
+		for (int contar = 1; contar <= contagem; contar++) {
+            System.out.println("Imprimindo numero " + contar);
+        }
+	}
+}
